72458930a04c2ef6cac1e1de067049b6888d8b65
AK Delfin <ak.delf@gmail.con>
2015-03-29 23:01:09 +0300

    add files

---
+++ genshared.php
@@ -0,0 +1,57 @@
+<?php
+
+	/**
+	* ShareGen
+	* NoScript support
+	*/
+
+	
+	/*
+	* controller 
+	*/
+	function genshared($list = 'all', $view = '') {
+
+		if ($view == '')
+			$view = 'views/horbuttons.phtml';
+
+		$buttons = gensharedlinks($list);
+		include $view;
+
+	}
+
+
+
+	/*
+	* model return social urls and javascript
+	*/
+
+	function gensharedlinks($list = 'all', $currlink = '') {
+
+		
+		if ($currlink == '')
+			$currlink = $_SERVER['PHP_SELF'];
+
+		
+		$slinks =  array(
+
+			'twitter' => 'https://twitter.com/intent/tweet?original_referer'.$currlink.'&amp;text=Мобильность&amp;tw_p=tweetbutton&amp;url='.$currlink.'&amp;via="><i class="fa fa-twitter"',
+			'facebook' => 'http://www.facebook.com/plugins/like.php?href=http://wordpress.my/2014/09/blue-steel/'.$currlink,
+			'google' => 'https://plus.google.com/share?url='.$currlink,
+			'linkedin' => 'http://www.linkedin.com/shareArticle?mini=true&amp;url='.$currlink,
+		
+		);
+
+		
+		if ($list == 'all')
+			return $slinks;
+		elseif (is_array($list)) {
+			foreach ($list as $item) {
+				if (in_array($item, $slinks))
+					$result = $share[$item];	
+			}
+		}
+
+		return $result;
+
+	
+}
